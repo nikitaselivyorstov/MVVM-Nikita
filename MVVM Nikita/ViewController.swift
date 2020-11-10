7fee529f437e517e8cd5345aa902006ea246730b
nikitaselivyorstov <nikitaseliverstov@gmail.com>
2020-11-10 14:37:43 +0300

    Test

---
+++ MVVM Nikita/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Nikita
+//
+//  Created by Nikita Seliverstov on 10.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
